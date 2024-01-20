8d463b751681d2a43a2fb21aa719d8d468b693ee
Ankit Ydv <ankityadav11241@gmail.com>
2024-01-20 21:48:56 +0530

    add: db method & loader

--- frontend/src/components/Auth/Auth.jsx
+++ frontend/src/components/Auth/Auth.jsx
@@ -1,18 +1,32 @@
 import "./Auth.css";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import {auth} from "../../config/auth_firebase"
+import { auth } from "../../config/auth_firebase";
+import { createUser } from "../../config/DB_API";
+import { useState } from "react";
+import Loader from "../Loader/Loader";
 
 export default function Auth() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSignIn = () => {
+    setIsLoading(true);
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        createUser(
+          user.uid,
+          user.providerData[0].displayName,
+          user.providerData[0].email,
+          user.providerData[0].photoURL
+        );
       })
       .catch((error) => {
         console.log("ERROR signing in");
         console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -21,12 +35,16 @@ export default function Auth() {
       <div className="authWrapper">
         <img src="/assets/logo.jpeg" alt="" />
         <span></span>
-        <div>
-          <img src="/assets/google-icon.png" alt="" />
-          <span className="authGoogleText" onClick={() => handleSignIn()}>
-            Sign In With Google
-          </span>
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="authGoogleBtn">
+            <img src="/assets/google-icon.png" alt="" />
+            <span className="authGoogleText" onClick={() => handleSignIn()}>
+              Sign In With Google
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
