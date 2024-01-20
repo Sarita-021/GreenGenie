15f6b44c08810e860f20c70b021f408bfd0f7402
Ankit Ydv <ankityadav11241@gmail.com>
2024-01-20 22:03:11 +0530

    middleware: morgan

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
