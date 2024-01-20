faa728e829074821ea6eec9dfa60e1efad869366
Ankit Ydv <ankityadav11241@gmail.com>
2024-01-20 21:48:32 +0530

    loader

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loading">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
