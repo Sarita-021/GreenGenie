965250b077af5af0ecf07c7420dd1f33e9400013
techmannih <manishchaudharyttt@gmail.com>
2024-01-21 18:48:39 +0530

    add env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-
+require("dotenv").config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 3001;
 
 const app = express();
