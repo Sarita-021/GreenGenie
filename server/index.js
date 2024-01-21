748ca28fab6935f11159452b6a10ed8a62cd43ee
Suniti-0020 <119940275+Suniti-0020@users.noreply.github.com>
2024-01-21 23:00:58 +0530

    Connected to database and changed the setting to run server and client concurrently.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
+const dotenv = require('dotenv')
+const { connect } = require('mongoose')
+const connectDB = require('./config/db')
 
-const PORT = process.env.PORT || 3001;
+
+//env config
+dotenv.config();
 
 const app = express();
+
+//middlewares
+app.use(cors())
+app.use(express.json())
 app.use(morgan("dev"));
 
+//mongodb connection 
+connectDB();
+
 app.get("/", (req, res) => {
-  res.send("Hello from server!");
+    res.send("Hello from server!");
 });
 
+const PORT = process.env.PORT || 8080
+
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+    console.log('server Running on ' + process.env.DEV_MODE + ' mode port no. ' + PORT)
+})
