2ea6f2bf80e92a16b14e1c0b27dd62373c7d3d0b
Ankit Ydv <ankityadav11241@gmail.com>
2024-01-22 21:19:04 +0530

    styled navbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,28 +1,58 @@
 import React from "react";
 import "../css/header.css";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+
+const navigation = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    name: "Education",
+    path: "/education",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
-    return (
-        <>
-            <nav>
-                <div className="logo">
-                    <img src="/assets/logo.jpeg" alt="" />
-                </div>
-                <ul>
-                    <li>Dashboard</li>
-                    <li>Education</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                </ul>
-                <div>
-                    <span>Login</span>
-                </div>
-            </nav>
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img src="/assets/logo.jpeg" alt="" />
+        </div>
+        <ul>
+          {navigation.map((item, index) => (
+            <li key={index}>
+              <NavLink to={item.path} activeClassName="active">
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <span><NavLink to="/login">Login</NavLink></span>
+        </div>
+      </nav>
 
-            <Outlet />
-        </>
-    )
+      <Outlet />
+    </>
+  );
 };
 
 export default Header;
