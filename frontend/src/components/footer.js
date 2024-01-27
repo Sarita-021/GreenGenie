de23bb92c9f75a2708feadd77d7386d0651a9872
techmannih <manishchaudharyttt@gmail.com>
2024-01-27 17:27:35 +0530

    add about page str

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,63 +1,96 @@
 import React from "react";
 import "../css/footer.css";
 import { Outlet } from "react-router-dom";
-import { FaFacebook, FaTwitter, FaGoogle, FaYoutube, FaLinkedin } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaGoogle,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <>
-            <div>
-                <h3>About us</h3>
-                <p>Donate, recycle, wear good. GreenGenie connects your closet to the community.</p>
-                <div className="icons">
-                    <ul className="socials">
-                        <li>
-                            <FaFacebook size={24} aria-label="Facebook" />
-                        </li>
-                        <li>
-                            <FaTwitter size={24} aria-label="Twitter" />
-                        </li>
-                        <li>
-                            <FaGoogle size={24} aria-label="Google" />
-                        </li>
-                        <li>
-                            <FaYoutube size={24} aria-label="YouTube" />
-                        </li>
-                        <li>
-                            <FaLinkedin size={24} aria-label="LinkedIn" />
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <>
+      <footer class="footer">
+        <div class="container">
+          <h3 className="footer-title">About us</h3>
+          <p className="p">
+            {" "}
+            <span>
+              Donate, recycle, wear good. GreenGenie connects your closet to the
+              community.
+            </span>
+          </p>
+          <div class="row">
+            <div class="footer-col">
+              <h4>Contact us</h4>
+
+              <p>Hisar, Haryana, INDIA</p>
+              <p>+91 8560XXXXXX</p>
+              <p>support@GreenGenie.com</p>
             </div>
-            <div>
-                <h3>Contact us</h3>
-                <p>Hisar, Haryana, INDIA</p>
-                <p>+91 8560XXXXXX</p>
-                <p>support@GreenGenie.com</p>
+            <div class="footer-col">
+              <h4>Links</h4>
+              <ul>
+                <li>
+                  <a to="/" className="footer-link">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a to="/about" className="footer-link">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a to="/blog" className="footer-link">
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <a to="/login" className="footer-link">
+                    Login
+                  </a>
+                </li>
+              </ul>
             </div>
-            {/* <div>
-                <h3>Quick Links</h3>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/blog">Blog</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div> */}
-            <p>Copyright © {new Date().getFullYear()} GreenGenie. All rights reserved.</p>
-            <Outlet />
-        </>
-    );
+            <div class="footer-col">
+              <h4>follow us</h4>
+              <div className="icons">
+                <ul className="socials">
+                  <li>
+                    <FaFacebook size={24} aria-label="Facebook" />
+                  </li>
+                  <li>
+                    <FaTwitter size={24} aria-label="Twitter" />
+                  </li>
+                  <li>
+                    <FaGoogle size={24} aria-label="Google" />
+                  </li>
+                  <li>
+                    <FaYoutube size={24} aria-label="YouTube" />
+                  </li>
+                  <li>
+                    <FaLinkedin size={24} aria-label="LinkedIn" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>{" "}
+          <p className="copy">
+            {" "}
+            <span className="footer-copyright">
+              Copyright © {new Date().getFullYear()} GreenGenie. All rights
+              reserved.
+            </span>
+          </p>
+        </div>
+      </footer>
+
+      <Outlet />
+    </>
+  );
 };
 
 export default Footer;
