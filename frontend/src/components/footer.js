a1721064e1ed52e63ef60c61e5b4370093928f60
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-02-09 20:30:10 +0530

    footer ui improve

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -13,79 +13,78 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
+        <p className="p">
+          {" "}
+          <span>
+            Donate, recycle, wear good. GreenGenie connects your closet to the
+            community.
+          </span>
+        </p>
         <div className="container">
-          <h3 className="footer-title">About us</h3>
-          <p className="p">
-            {" "}
-            <span>
-              Donate, recycle, wear good. GreenGenie connects your closet to the
-              community.
-            </span>
-          </p>
-          <div className="row">
-            <div className="footer-col">
-              <h4>Contact us</h4>
+          {/* <div className="row"> */}
+          <div className="footer-col">
+            <h4>Links</h4>
+            <ul>
+              <li>
+                <a to="/" className="footer-link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a to="/about" className="footer-link">
+                  About
+                </a>
+              </li>
+              <li>
+                <a to="/blog" className="footer-link">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a to="/login" className="footer-link">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Contact us</h4>
 
-              <p>Hisar, Haryana, INDIA</p>
-              <p>+91 8560XXXXXX</p>
-              <p>support@GreenGenie.com</p>
-            </div>
-            <div className="footer-col">
-              <h4>Links</h4>
-              <ul>
+            <p>Hisar, Haryana, INDIA</p>
+            <p>+91 8560XXXXXX</p>
+            <p>support@GreenGenie.com</p>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="icons">
+              <ul className="socials">
                 <li>
-                  <a to="/" className="footer-link">
-                    Home
-                  </a>
+                  <FaFacebookF size={20} aria-label="Facebook" />
                 </li>
                 <li>
-                  <a to="/about" className="footer-link">
-                    About
-                  </a>
+                  <FaTwitter size={20} aria-label="Twitter" />
                 </li>
                 <li>
-                  <a to="/blog" className="footer-link">
-                    Blog
-                  </a>
+                  <FaGoogle size={20} aria-label="Google" />
                 </li>
                 <li>
-                  <a to="/login" className="footer-link">
-                    Login
-                  </a>
+                  <FaYoutube size={20} aria-label="YouTube" />
+                </li>
+                <li>
+                  <FaLinkedinIn size={20} aria-label="LinkedIn" />
                 </li>
               </ul>
             </div>
-            <div className="footer-col">
-              <h4>follow us</h4>
-              <div className="icons">
-                <ul className="socials">
-                  <li>
-                    <FaFacebookF size={24} aria-label="Facebook" />
-                  </li>
-                  <li>
-                    <FaTwitter size={24} aria-label="Twitter" />
-                  </li>
-                  <li>
-                    <FaGoogle size={24} aria-label="Google" />
-                  </li>
-                  <li>
-                    <FaYoutube size={24} aria-label="YouTube" />
-                  </li>
-                  <li>
-                    <FaLinkedinIn size={24} aria-label="LinkedIn" />
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>{" "}
-          <p className="copy">
-            {" "}
-            <span className="footer-copyright">
-              Copyright © {new Date().getFullYear()} GreenGenie. All rights
-              reserved.
-            </span>
-          </p>
-        </div>
+          </div>
+        </div>{" "}
+        {/* </div> */}
+        <p className="copy-right">
+          {" "}
+          <span className="footer-copyright">
+            Copyright © {new Date().getFullYear()} GreenGenie. All rights
+            reserved.
+          </span>
+        </p>
       </footer>
 
       <Outlet />
